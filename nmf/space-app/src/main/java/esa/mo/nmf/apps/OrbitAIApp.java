11530a70a3ecfcf768664ab2c896f821455a8c49
Tanguy Soto <tanguy.soto@esa.int>
2021-02-08 13:24:37 +0100

    NMF-APP: consuming PD and quaternions parameters from supervisor

--- nmf/space-app/src/main/java/esa/mo/nmf/apps/OrbitAIApp.java
+++ nmf/space-app/src/main/java/esa/mo/nmf/apps/OrbitAIApp.java
@@ -1,5 +1,8 @@
 package esa.mo.nmf.apps;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import esa.mo.helpertools.misc.Const;
 import esa.mo.nmf.nanosatmoconnector.NanoSatMOConnectorImpl;
 
 /**
@@ -8,6 +11,43 @@ import esa.mo.nmf.nanosatmoconnector.NanoSatMOConnectorImpl;
  * @author Tanguy Soto
  */
 public class OrbitAIApp {
+
+  private static final Logger LOGGER = Logger.getLogger(OrbitAIApp.class.getName());
+
+  // Providers
+  private NanoSatMOConnectorImpl connector;
+  private OrbitAIMCAdapter adapter;
+
+  public OrbitAIApp() {
+    //System.setProperty(Const.CENTRAL_DIRECTORY_URI_PROPERTY,
+    //    "maltcp://10.0.2.15:1024/nanosat-mo-supervisor-Directory");
+
+    connector = new NanoSatMOConnectorImpl();
+    adapter = new OrbitAIMCAdapter(this);
+    connector.init(adapter);
+
+    LOGGER.log(Level.INFO, "OrbitAI initialized.");
+  }
+
+  /**
+   * Returns the connector.
+   * 
+   * @return a NanoSatMOConnectorImpl containing the connector of this OrbitAIApp
+   */
+  public NanoSatMOConnectorImpl getConnector() {
+    return connector;
+  }
+
+  /**
+   * Returns the adapter.
+   * 
+   * @return a OrbitAIMCAdapter containing the adapter of this OrbitAIApp
+   */
+  public OrbitAIMCAdapter getAdapter() {
+    return adapter;
+  }
+
+
   /**
    * Main command line entry point.
    *
@@ -15,8 +55,6 @@ public class OrbitAIApp {
    * @throws java.lang.Exception If there is an error
    */
   public static void main(final String args[]) throws Exception {
-    final NanoSatMOConnectorImpl connector = new NanoSatMOConnectorImpl();
-    OrbitAIMCAdapter adapter = new OrbitAIMCAdapter(connector);
-    connector.init(adapter);
+    OrbitAIApp app = new OrbitAIApp();
   }
 }
--- nmf/space-app/src/main/java/esa/mo/nmf/apps/OrbitAIMCAdapter.java
+++ nmf/space-app/src/main/java/esa/mo/nmf/apps/OrbitAIMCAdapter.java
@@ -1,8 +1,13 @@
 package esa.mo.nmf.apps;
 
 import esa.mo.nmf.MonitorAndControlNMFAdapter;
-import esa.mo.nmf.NMFInterface;
 import esa.mo.nmf.annotations.Parameter;
+import esa.mo.nmf.commonmoadapter.SimpleDataReceivedListener;
+import esa.mo.nmf.spacemoadapter.SpaceMOApdapterImpl;
+import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,21 +19,126 @@ public class OrbitAIMCAdapter extends MonitorAndControlNMFAdapter {
 
   private static final Logger LOGGER = Logger.getLogger(OrbitAIMCAdapter.class.getName());
 
-  private NMFInterface connector;
+  private static final float PARAMS_DEFAULT_VALUE = 42;
+
+  // Reference to the parent NMF app
+  private OrbitAIApp app;
+
+
+  // Supervisor consumers
+  private SpaceMOApdapterImpl supervisorSMA;
 
   // ----------------------------------- Parameters -----------------------------------------------
 
-  @Parameter(description = "are we exposed to the sun (test parameter)", generationEnabled = true,
-      reportIntervalSeconds = 2)
-  private boolean isExposedToSun = false;
+  // -------- RE-EXPOSING parameters consumed from supervisor --------
+
+  // CADC
+
+  @Parameter(description = "I_OM_FB_FI_FB_0 fetched from supervisor", generationEnabled = true,
+      reportIntervalSeconds = 5, readOnly = true)
+  private float CADC0880 = PARAMS_DEFAULT_VALUE;
+
+  @Parameter(description = "I_OM_FB_FI_FB_0 fetched from supervisor", generationEnabled = true,
+      reportIntervalSeconds = 5, readOnly = true)
+  private float CADC0881 = PARAMS_DEFAULT_VALUE;
+
+  @Parameter(description = "I_OM_FB_FI_FB_0 fetched from supervisor", generationEnabled = true,
+      reportIntervalSeconds = 5, readOnly = true)
+  private float CADC0882 = PARAMS_DEFAULT_VALUE;
 
 
-  public OrbitAIMCAdapter(final NMFInterface connector) {
-    this.connector = connector;
-    LOGGER.log(Level.INFO, "OrbitAI space application initialized.");
+  // CADC PHOTODIOES parameters
+
+  @Parameter(description = "I_PD1_THETA fetched from supervisor", generationEnabled = true,
+      reportIntervalSeconds = 5, readOnly = true)
+  private float CADC0884 = PARAMS_DEFAULT_VALUE;
+
+  @Parameter(description = "I_PD2_THETA fetched from supervisor", generationEnabled = true,
+      reportIntervalSeconds = 5, readOnly = true)
+  private float CADC0886 = PARAMS_DEFAULT_VALUE;
+
+  @Parameter(description = "I_PD3_THETA fetched from supervisor", generationEnabled = true,
+      reportIntervalSeconds = 5, readOnly = true)
+  private float CADC0888 = PARAMS_DEFAULT_VALUE;
+
+  @Parameter(description = "I_PD4_THETA fetched from supervisor", generationEnabled = true,
+      reportIntervalSeconds = 5, readOnly = true)
+  private float CADC0890 = PARAMS_DEFAULT_VALUE;
+
+  @Parameter(description = "I_PD5_THETA fetched from supervisor", generationEnabled = true,
+      reportIntervalSeconds = 5, readOnly = true)
+  private float CADC0892 = PARAMS_DEFAULT_VALUE;
+
+  @Parameter(description = "I_PD6_THETA fetched from supervisor", generationEnabled = true,
+      reportIntervalSeconds = 5, readOnly = true)
+  private float CADC0894 = PARAMS_DEFAULT_VALUE;
+
+  // CADC QUATERNION parameters
+
+  @Parameter(description = "O_Q_FB_FI_EST_0 fetched from supervisor", generationEnabled = true,
+      reportIntervalSeconds = 5, readOnly = true)
+  private float CADC1002 = PARAMS_DEFAULT_VALUE;
+
+  @Parameter(description = "O_Q_FB_FI_EST_1 fetched from supervisor", generationEnabled = true,
+      reportIntervalSeconds = 5, readOnly = true)
+  private float CADC1003 = PARAMS_DEFAULT_VALUE;
+
+  @Parameter(description = "O_Q_FB_FI_EST_2 fetched from supervisor", generationEnabled = true,
+      reportIntervalSeconds = 5, readOnly = true)
+  private float CADC1004 = PARAMS_DEFAULT_VALUE;
+
+  @Parameter(description = "O_Q_FB_FI_EST_3 fetched from supervisor", generationEnabled = true,
+      reportIntervalSeconds = 5, readOnly = true)
+  private float CADC1005 = PARAMS_DEFAULT_VALUE;
+
+
+  public OrbitAIMCAdapter(OrbitAIApp app) {
+    this.app = app;
+    this.supervisorSMA = SpaceMOApdapterImpl
+        .forNMFSupervisor(this.app.getConnector().readCentralDirectoryServiceURI());
+
+    startQueryingSupervisorParameters();
+
+    LOGGER.log(Level.INFO, "OrbitAIMCAdapter initialized.");
   }
 
-  public NMFInterface getConnector() {
-    return connector;
+  /**
+   * Queries OBSW parameters values from the supervisor and set our internal parameters with those
+   * values.
+   *
+   */
+  private void startQueryingSupervisorParameters() {
+    // Toggle parameters generation in supervisor
+    List<String> parameterNames = new ArrayList<String>();
+    for (Field f : this.getClass().getDeclaredFields()) {
+      if (f.getName().startsWith("CADC")) {
+        parameterNames.add(f.getName());
+      }
+    }
+    supervisorSMA.toggleParametersGeneration(parameterNames, true);
+
+    // Listens for parameters values from supervisor
+    supervisorSMA.addDataReceivedListener(new SimpleDataReceivedListener() {
+      @Override
+      public void onDataReceived(String parameterName, Serializable data) {
+        String dataS = data.toString();
+        try {
+          Field internalParameter =
+              OrbitAIMCAdapter.this.getClass().getDeclaredField(parameterName);
+          internalParameter.setAccessible(true);
+          internalParameter.set(OrbitAIMCAdapter.this, Float.parseFloat(dataS));
+          internalParameter.setAccessible(false);
+        } catch (NoSuchFieldException e) {
+          // received parameter that we don't need
+          LOGGER.log(Level.WARNING, String
+              .format("Received unused value for parameter %s from supervisor", parameterName));
+        } catch (SecurityException | IllegalArgumentException | IllegalAccessException e1) {
+          LOGGER.log(Level.SEVERE,
+              "Error setting internal parameter from supervisor acquired value", e1);
+        }
+        LOGGER.log(Level.INFO, String.format("Received value %s from supervisor for parameter %s",
+            dataS, parameterName));
+      }
+    });
   }
 }
