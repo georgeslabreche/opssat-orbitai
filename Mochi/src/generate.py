743dc1f3486e68389f78835f73e127be3631204a
Georges Labrèche <georges@subspacesignal.com>
2021-03-11 22:00:23 -0500

    update data analysis scripts to include new model outputs

--- Mochi/src/generate.py
+++ Mochi/src/generate.py
@@ -12,7 +12,7 @@ From the Mochi root directory:
 4. Check that training log file was created: logs/training.csv
 5. Infer spacecraft data fetched from WebMUST: eval 'echo "load"; sleep 1; input="test_data/camera_validation_small.txt"; while IFS= read -r line; do sleep 0.02; echo "infer ${line/+/''} $(date +%s%3N)"; done < "$input";' | telnet localhost 9999
 6. Check that inference log file was creatd: logs/training.csv
-7. Calculate inference/prediction accuracies: python3 src/accurancy.py
+7. Calculate inference/prediction accuracies: python3 src/accuracy.py
 
 '''
 
@@ -24,13 +24,14 @@ PD_ELEVATION_THRESHOLD_HD_CAM = 1.0472
 FLOOR = 0.00 # 0 radians.
 CEILING = 1.57 # 90 degrees in radian.
 
-pd = FLOOR
+pd6 = FLOOR
 
 # Start time in milliseconds.
 # To simulate data input for logging photodiode value acquisition time.
 start_time = int(time.time() * 1000)
 
 increment = True
+include_decrement_data = True
 
 with open("test_data/generated.txt", "w") as data_file: 
 
@@ -38,35 +39,39 @@ with open("test_data/generated.txt", "w") as data_file:
     while True:
 
         # Make sure training photodiode data only has 2 digits after the floating point.
-        pd = round(pd, 2)
+        pd6 = round(pd6, 2)
 
-        if pd >= 0:
+        if pd6 >= 0:
 
             # Label '1' means camera can be turned ON.
             # Label '-1' means camera must be turned OFF.
-            label = 1 if pd < PD_ELEVATION_THRESHOLD_HD_CAM else -1
+            label = 1 if pd6 < PD_ELEVATION_THRESHOLD_HD_CAM else -1
 
-            # Write generated trianing command.
-            line = 'train ' + str(label) + ' ' + str(pd) + ' ' + str(start_time) + '\n'
+            # Generate train command and write it.
+            # Use dummy values for pd1 through pd5.
+            line = 'train ' + str(label) + ' 0.11 0.22 0.33 0.44 0.55 ' + str(pd6) + ' ' + str(start_time) + '\n'
             data_file.write(line)
         
         # Exit loop after decrement phase is done.
-        if pd < 0:
+        if pd6 < 0:
             break
 
         # Increment phase.
-        elif pd < CEILING and increment:
+        elif pd6 < CEILING and increment:
 
             # Increment photodiode elevation angle value.
-            pd += 0.01
+            pd6 += 0.01
 
         # Decrement phase.
-        else:
+        elif include_decrement_data:
             # Start decrementing.
             increment = False
             
             # Decrement photodiode elevation angle value.
-            pd -= 0.01
+            pd6 -= 0.01
+
+        else:
+            break
 
         # Photodiode data acquisition is planned for every 5 seconds.
         start_time += 5000 
