734b03b500a2afa54c7e135fc938d5503bbaabda
Georges Labrèche <georges@subspacesignal.com>
2021-05-17 21:16:59 -0400

    included models and their classification metrics for models trained up to 2021-05-15

--- Mochi/tools/README.md
+++ Mochi/tools/README.md
@@ -6,3 +6,21 @@ A collection of scripts to generate test commands, calculate classification metr
 - **inference.sh**: Run inference commands on trained models and calculate their performance metrics.
 
 Instructions on how to use this scripts are commented in their respective source files.
+
+## Evaluating models trained in space
+From this `tools` directory:
+
+1. Put the downlinked model files inside a yyyy-MM-dd folder in the `models_space` directory.
+2. Prefix the model files' content with `22 serialization::archive 15 `. This prefix is used when serializing/deserializing model files on the ground in a 64-bit environment but not on the spacecraft's ARM 32-bit environment. The prefix needs to be included for the models to load correctly when evalutating them on a 64-bit environment.
+3. Delete the `models` directory and recreate it with the model files from steps 1⁠–2. E.g.: 
+```bash
+rm -r models
+mkdir models
+cp -r models_space/2021-05-15/* models/
+```
+4. Execute the script that will load the models and run inferences using the test inference commands. Be sure to appropriately timestamp the output CSV file for the classfication metrics. E.g.: 
+```bash
+./inference.sh 0 0.05 cmds/inferences_A.txt metrics/space/inferences_2021-05-15.csv
+```
+
+Note that only 1D, 2D, and 3D models are evaluated from the ground because there are not test commands with 5D inference inputs in `cmds/inferences_A.txt`. Test 5D inference inputs can be compiled from the experiment's log files on-board the spacecraft or inference can be directly evaluated in space.
--- Mochi/tools/inference.sh
+++ Mochi/tools/inference.sh
@@ -6,20 +6,23 @@
 # The cmds/inferences_A.txt file used in this bash script was manually created by copying cmds/generated_A.txt and
 # removing all training commands so that only inference commands are listed.
 
-# Usage: ./inference.sh <float:sleep> <string:commands_filename>
+# Usage: ./inference.sh <int:analyze_5d_flag> <float:sleep> <string:commands_filename> <string:csv_results_filname>
+#   - analyze_5d_flag: Flag to indicate if classification metrics should be calculated for models trained in 5D input space.
+#                      1 for True or 0 for False.
 #   - sleep: sleep time between each commands sent.
 #   - commands_filename: path to the file listing all the commands to send to the Mochi server.
+#   - csv_results_filname: path of the CSV file in which the inference results will be written.
 #
-# e.g.: ./inference.sh 0.03 cmds/inferences_A.txt
+# e.g.: ./inference.sh 0 0.03 cmds/inferences_A.txt metrics/space/inferences_2021-05-15.csv
 
-if [ $# -ne 2 ]; then
+if [ $# -ne 4 ]; then
     echo "Invalid number of parameters. Usage: ./inference.sh <float:sleep> <string:commands_filename>"
     exit 1
 fi
 
 # Clear the logs, for good measure.
-rm logs/inference.csv
-rm logs/training.csv
+rm -r logs
+mkdir logs
 
 PORT=9999
 
@@ -30,10 +33,10 @@ PORT=9999
 sleep 1
 
 # Load models and infer.
-eval 'echo "load"; sleep 1; input="$2"; while IFS= read -r cmd; do sleep $1; echo ${cmd}; done < "$input"; echo "exit"' | telnet localhost $PORT
+eval 'echo "load"; sleep 1; input="$3"; while IFS= read -r cmd; do sleep $2; echo ${cmd}; done < "$input"; echo "exit"' | telnet localhost $PORT
 
 # Caculate classification metrics of the trained models.
-python3 analyze.py 1 logs/inference.csv metrics/space/inferences_2021-04-18.csv
+python3 analyze.py $1 logs/inference.csv $4
 
 
 
